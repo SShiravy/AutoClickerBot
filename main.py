74559c63850c2bf9c493217e305edf455e7dbebb
SoheilShiravy <soheilprogrammer0@gmail.com>
2024-06-05 18:41:29 +0330

    initial structure and plan to code.

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# TODO : get position of cursor on screen / auto click on that position on desktop: 'Clks' state get the limit number
+#  of click or time period and click until limit :'ClksLimit' state add randomize to Clks state ability to take a
+#  path after ClksLimit state , to another airdrop bot and another account : 'ChngBot' state and 'ChngAccount' State
+#  handle queue of states and timing
+#  add GUI
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('where should i click for you?')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
