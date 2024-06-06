ccf0f97c4c29eff78067028b0ab557ef039be3ae
SoheilShiravy <soheilprogrammer0@gmail.com>
2024-06-06 19:00:57 +0330

    now we can easily set path and a center with its corner path will take as steps and center will click multiple times. EveryThing is OK

---
+++ config.py
@@ -0,0 +1,10 @@
+SLEEP_CLICK = 0.15  # s / sleep time after each click
+SLEEP_STEP = 0.3  # s / sleep time after each step !! it will add up to SLEEP_CLICK
+
+# -------------------- BOT SETTING --------------------------
+
+N_ACCOUNT = 2  # how many account is there to use / just to know
+N_CLICKING_AIR_DROPS = 2  # how many clicking airdrop is there? / just to know
+T_MAX_RUN = []  # maximum time of running bot
+N_CLKS_EARN_LIST = []  # number of earn click for each clicking airdrop
+T_CLKS_EARN_LIST = []  # period time of earn click for each clicking airdrop
--- main.py
+++ main.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# TODO : add escape key ,add log, to another airdrop bot and another account : 'ChngBot' state and
+#  'ChngAccount' State handle queue of states and timing add GUI
 
-# TODO : get position of cursor on screen / auto click on that position on desktop: 'Clks' state get the limit number
-#  of click or time period and click until limit :'ClksLimit' state add randomize to Clks state ability to take a
-#  path after ClksLimit state , to another airdrop bot and another account : 'ChngBot' state and 'ChngAccount' State
-#  handle queue of states and timing
-#  add GUI
+# Naming Conventions : modules are camelCase, classes are PascalCase and variables and functions are snake_case
 
-# Press the green button in the gutter to run the script.
+
+from pynput import keyboard
+from clksAirDrops import ClksAirDrops
+
 if __name__ == '__main__':
-    print('where should i click for you?')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    clks_bot = ClksAirDrops('test')
+
+    def on_press(key):
+        """
+        :param key: pynput.keyboard.Key type
+        :return: bool
+        """
+        # initialization -------------------------------------
+
+        if key == keyboard.Key.shift_l:
+            print('--- set path to earn button/ right click for ending')
+            clks_bot.set_path_to_earn()
+            print('--- set center of earn button/ right click for ending')
+            clks_bot.set_clks_pos()
+            print('--- set corner of earn button/ right click for ending')
+            clks_bot.set_corner_clks_pos()
+            print('--- set path to out/ right click for ending')
+            clks_bot.set_path_to_out()
+            n = input('---- enter number of clicks/ process will be start, !!! check the start screen')
+            clks_bot.go_to_earn()
+            clks_bot.earn_coin(n_times=int(n))
+            clks_bot.go_out()
+
+        elif key == keyboard.Key.esc:
+            keyboardListener.stop()
+
+    with keyboard.Listener(
+            on_press=on_press,
+            on_release=None) as keyboardListener:
+        keyboardListener.join()
+
+    print(clks_bot.cursor_control.elapsed_time)
+
